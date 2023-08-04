385fb5e056189219cabdab962f339bc2eb4384c5
HANAH-J <hana21k@naver.com>
2023-08-04 22:17:18 +0900

    design: 230804_change_Alert
    헤더 로그아웃 알림창 활성화 시 배경 크기 추가

--- reelreview/src/components/users/SignOutAlert.jsx
+++ reelreview/src/components/users/SignOutAlert.jsx
@@ -18,7 +18,7 @@ export default function SignOutAlert({ setShowSignOutAlert, SignOutHeader }) {
 
     return (
         <div>
-            <div className={styles.modalBackground2} style={{ backgroundColor: "black" }} />
+            <div className={SignOutHeader === 'SignOutHeader' ? styles.modalBackground5 : styles.modalBackground2} style={{ backgroundColor: "black" }} />
             <div className={SignOutHeader === 'SignOutHeader' ? styles.signOut_alert : styles.signOut_alert2}>
                 <h2 className={styles.alert_h2}>알림</h2>
                 <p className={styles.alert_p}>로그아웃 하시겠어요?</p>
